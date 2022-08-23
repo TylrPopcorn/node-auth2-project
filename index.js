c189fcd038ec9c0a6a72774ca0a22b6f7ab5c981
Tyler <tyler_mac@Rainbow-Trains-MacBook-Pro.local>
2022-08-23 10:35:59 -0400

    Followed along with the teacher and learned.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/*
+  DEPENDENCIES
+
+  npm install
+  npm i dotenv
+  npm i jsonwebtoken
+
+  npm i -g fkill-cli
+
+*/
+
+require("dotenv").config()
+
 const server = require('./api/server.js');
 
 const PORT = process.env.PORT || 9000;
